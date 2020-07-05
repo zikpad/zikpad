bfb2973b1d840f77fa55b49534efbc96c3a8f422
François Schwarzentruber <francois.schwarzentruber@ens-rennes.fr>
2020-07-05 16:33:19 +0200

    overflow

--- src/Score.ts
+++ src/Score.ts
@@ -60,7 +60,7 @@ function drawLines() {
     }
 
     //measure lines
-    for (let t = 0; t < 30; t++) {
+    for (let t = 1; t < 30; t++) {
         let x = Layout.getX(t) - 2 * Layout.NOTERADIUS;
         Drawing.line(x, Layout.getY(-10), x, Layout.getY(10));
     }
